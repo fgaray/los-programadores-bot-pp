ef0abd67f827d21d65af156e69177703c48615f3
Felipe Garay <felipe.garay@usach.cl>
2020-09-29 21:37:19 -0300

    First compilation of the bot

--- bot/main.cc
+++ bot/main.cc
@@ -1,7 +1,30 @@
 #include <iostream>
+#include "tgbot/tgbot.h"
 
 
 int main() {
-  std::cout << "Hello" << std::endl;
+  const char* token = std::getenv("TOKEN");
+  if (!token) {
+    std::cerr << "Set the TOKEN enviroment variable" << std::endl;
+    return -1;
+  }
+  TgBot::Bot bot(token);
+
+  bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+    std::cout << "ECHO " << message->text << std::endl;
+  });
+
+
+  // run the bot
+  try {
+    std::cout << "Running..." << std::endl;
+    TgBot::TgLongPoll poll(bot);
+    while (true) {
+      poll.start();
+    }
+  } catch (TgBot::TgException& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
   return 0;
 }
